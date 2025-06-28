6324951fa38ca1d157faf8aababe9e4120f1f1d5
Olof Johansson <olof@liberdev.se>
2025-06-28 19:59:37 +0200

    Add filters

--- wordpress-security-goodies.php
+++ wordpress-security-goodies.php
@@ -7,7 +7,7 @@
  * Author URI:      https://oljo.online
  * Text Domain:     wsg
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.0.1
  *
  * @package         Wp_security_goodies
  */
@@ -16,5 +16,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Disable XML-RPC
+ *
+ * This filter disables the XML-RPC functionality in WordPress.
+ * It is a common security measure to prevent brute force attacks
+ * and other vulnerabilities associated with XML-RPC.
+ */
+add_filter( 'xmlrpc_methods', function ( $methods ) {
+  return [];
+});
 
+/**
+ * Disable REST API for non-logged-in users
+ */
+add_filter( 'rest_authentication_errors', function( $result ) {
+    // If a previous authentication check was applied,
+    // pass that result along without modification.
+    if ( true === $result || is_wp_error( $result ) ) {
+        return $result;
+    }
+
+    // No authentication has been performed yet.
+    // Return an error if user is not logged in.
+    if ( ! is_user_logged_in() ) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            __( 'You are not currently logged in.' ),
+            array( 'status' => 401 )
+        );
+    }
+
+    // Our custom authentication check should have no effect
+    // on logged-in requests
+    return $result;
+});
 
